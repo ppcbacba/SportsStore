27d9827f9d9dfcba61286af3d8f67aab79e43bf1
qlzcl <qlzcl@LAPTOP-1JS5KM9I>
2019-09-10 07:06:31 +0800

    建立数据库表

---
+++ SportsStore.Domain/sql script/dbo.Products.data.sql
@@ -0,0 +1,11 @@
+﻿SET IDENTITY_INSERT [dbo].[Products] ON
+INSERT INTO [dbo].[Products] ( [Name], [Description], [Category], [Price]) VALUES , N'Kayak', N'A boat for one person', N'WaterSports', CAST(275.00 AS Decimal(16, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [Category], [Price]) VALUES , N'Liftjacket', N'Protective and fashionable', N'WaterSports', CAST(48.95 AS Decimal(16, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [Category], [Price]) VALUES , N'Soccer Ball', N'FIFA-approved size and wight', N'Soccer', CAST(19.50 AS Decimal(16, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [Category], [Price]) VALUES , N'CornerFlag', N'Give you playing field a professional touch', N'Soccer', CAST(34.95 AS Decimal(16, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [Category], [Price]) VALUES , N'Stadium', N'Flat-packed 35000-seat stadium', N'Socder', CAST(79500.00 AS Decimal(16, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [Category], [Price]) VALUES , N'Thinking Cap', N'Improve your brain efficeency by 75%', N'Chess', CAST(16.00 AS Decimal(16, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [Category], [Price]) VALUES , N'Unsteady Chair', N'Secretyl give your opponent a disadvantage ', N'Chess', CAST(29.95 AS Decimal(16, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [Category], [Price]) VALUES , N'Human Chess Board', N'A fun game for the family', N'Chess', CAST(75.00 AS Decimal(16, 2)))
+INSERT INTO [dbo].[Products] ( [Name], [Description], [Category], [Price]) VALUES , N'Bling-Bling King', N'Gold plated,diamond-studded King', N'Chess', CAST(1200.00 AS Decimal(16, 2)))
+SET IDENTITY_INSERT [dbo].[Products] OFF
